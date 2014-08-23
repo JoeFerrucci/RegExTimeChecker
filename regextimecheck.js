278aaea5cfd7342c1bdfe82747c3faecfc1c65d6
Joe Ferrucci <JoeFerrucci@users.noreply.github.com>
2014-08-23 13:04:08 -0700

    Add example

--- regextimecheck.js
+++ regextimecheck.js
@@ -3,3 +3,9 @@ var regex = /^([0-1]\d):([0-5]\d)\s*(?:AM|PM)?$/i;
 function testDateRegex(rx) {
     return regex.test(rx);
 }
+
+
+/* Example 1 */
+var isThisTimeValid = '06:30PM';
+if ( testDateRegex(isThisTimeValid) )
+    alert('The time ' + isThisTimeValid + ' is valid!');
