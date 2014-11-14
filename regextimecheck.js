3be117a2f55d21fc083ab61b23b6de5aceccde59
Joe Ferrucci <JoeFerrucci@users.noreply.github.com>
2014-11-14 10:07:22 -0800

    Removed unnecessary code.

--- regextimecheck.js
+++ regextimecheck.js
@@ -1,11 +1,7 @@
 /* Regex Date Checker */
 var regex = /^([0-1]\d):([0-5]\d)\s*(?:AM|PM)?$/i;
-function testDateRegex(rx) {
-    return regex.test(rx);
-}
-
 
 /* Example 1 */
 var isThisTimeValid = '06:30PM';
-if ( testDateRegex(isThisTimeValid) )
+if ( regex.test(isThisTimeValid) )
     alert('The time ' + isThisTimeValid + ' is valid!');
